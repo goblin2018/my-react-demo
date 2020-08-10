ba6b24a1f5ac7fc452cfd33cd8539737f050dfcf
goblin2018 <zhangyl2015@qq.com>
2020-08-10 23:30:05 +0800

    add redux

---
+++ src/redux/index.ts
@@ -0,0 +1,7 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import { routinePromiseWatcherSaga } from 'redux-saga-routines'
+
+import createSageMiddleware from 'redux-saga'
+import { createBrowserHistory } from 'history'
+import thunk from 'redux-thunk'
+import { routerMiddleware } from 'connected-react-router'
